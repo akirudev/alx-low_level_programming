eb0447a8e1e4ad1b14293020726a5d6d49ccc337
Hamza <you@example.com>
2023-07-13 23:57:29 -0700

    🚀

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - print white (puts)
+ *
  * Return: zero
  */
-int	main(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");;
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
