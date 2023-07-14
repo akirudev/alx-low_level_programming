f77c29268887cec73e1503cef8708d5d0f15674d
Hamza <you@example.com>
2023-07-13 18:55:41 -0700

    🚀

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print white (printf)
+ * Return: zero
+ */
+int	main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
